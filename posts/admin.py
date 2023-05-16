44fd82774f300645fc28fadcda260b6e855103cf
Shafiul Islam-Mehdi <shafiul.islam-mehdi@kcl.ac.uk>
2023-05-16 16:56:50 +0100

    Configure basic admin interface

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import User
 
 # Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+
+    """Admin interface config for users"""
+
+    list_display = [
+        'username',
+        'email',
+        'first_name',
+        'last_name',
+        'is_active'
+    ]
