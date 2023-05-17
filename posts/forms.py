903799cbe3e1379efdb39a3894b1bcb59425ea52
Shafiul Islam-Mehdi <shafiul.islam-mehdi@kcl.ac.uk>
2023-05-17 18:24:53 +0100

    Refactor sign up form
    - Add regex to check password format
    - Add tests for sign up form

--- posts/forms.py
+++ posts/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.core.validators import RegexValidator
 from .models import User
 
 class SignUpForm(forms.ModelForm):
@@ -7,5 +8,13 @@ class SignUpForm(forms.ModelForm):
         fields = ['first_name', 'last_name', 'username', 'email', 'bio']
         widgets = {'bio': forms.Textarea()}
     
-    new_password = forms.CharField(label='Password', widget=forms.PasswordInput())
+    password = forms.CharField(
+        label='Password',
+        widget=forms.PasswordInput(),
+        validators=[RegexValidator(
+            regex=r'^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9]).*$',
+            message='Password must contain an uppercase character, a lowercase '
+                    'character and a number'
+            )]
+    )
     confirm_password = forms.CharField(label='Confirm Password', widget=forms.PasswordInput())
